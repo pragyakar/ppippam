7fc5e88e5f144d9c04f2ca4651937fd77b78541e
pragyakar <pragyakar75@gmail.com>
2021-03-07 19:47:09 +0545

    :package: Added react-icons dependency

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -2,6 +2,7 @@ const Landing = () => {
   return (
     <div className="landing-container">
       <span className="home-logo">ppippam</span>
+      <span className="home-description">Gallery of assorted landing page design inspiration</span>
     </div>
   );
 }
